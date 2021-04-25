765bcc70a39a300e6025ed8abf74b6e4068f65e0
xlzy <1017866168@qq.com>
2021-04-25 19:09:08 +0800

    u: 统一封装db请求

--- src/api/common.js
+++ src/api/common.js
@@ -1,56 +1,44 @@
-import _request from '@/utils/request'
+// import _request from '@/utils/request'
 
-const baseUrl = '/list/'
-const request = (url, ...arg) => _request(baseUrl + url, ...arg)
 const db = wx.cloud.database()
 
+const errorMessageError = {
+  '-502005': '数据库或数据表不存在',
+}
+
+export const dbRequest = (collectionName, actions = []) => new Promise(((resolve, reject) => {
+  let baseService = db.collection(collectionName)
+  if (!actions.length) {
+    baseService = baseService.get()
+  }
+  for (let i = 0; i < actions.length; i++) {
+    const item = actions[i]
+    const { method, condition } = item
+    if (!condition) {
+      baseService = baseService[method]()
+    } else {
+      baseService = baseService[method](condition)
+    }
+  }
+  baseService.then(res => {
+    console.log(res)
+    resolve(res)
+  }).catch(err => {
+    const msg = errorMessageError[err.errCode] || ''
+    uni.showToast({ title: `请求失败,${msg}`, icon: 'none' })
+    console.table(err)
+    reject(err)
+  }).finally(() => {
+
+  })
+}))
+
 export default {
-  getPhotoList(){
-    db.collection('photo').get().then(res=>{
+  getBlogList() {
+    db.collection('blog').get().then(res => {
       console.log(res)
       return res
     })
-  },
-  getVideoList(){
-    db.collection('video').get().then(res=>{
-      console.log(res)
-      return res
-    })
-  },
-  getBlogList(){
-    db.collection('blog').get().then(res=>{
-      console.log(res)
-      return res
-    })
-  },
-  
-  list(data) {
-    // mock
-    return new Promise((resolve, reject) => {
-      const data = {
-        list: [
-          { img: 'http://dummyimage.com/212x212/FF6600', title: '环球科学', desc: 21 },
-          { img: 'http://dummyimage.com/212x212/FF6600', title: '媒体视点', desc: 21 },
-          { img: 'http://dummyimage.com/212x212/FF6600', title: '科技知道', desc: 21 },
-          { img: 'http://dummyimage.com/212x212/FF6600', title: '管窥天下', desc: 21 }
-        ],
-        total: 4,
-        pageNum: 1,
-      }
-      setTimeout(() => {
-        resolve(data)
-      }, 600)
-    })
-    return request('list', data, 'get')
-  },
-  save(data) {
-    return request('save', data)
-  },
-  detail(data) {
-    return request('detail', data)
-  },
-  del(data) {
-    return request('del', data)
   },
 
 }
