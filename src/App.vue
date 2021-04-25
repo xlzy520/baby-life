dc05faaf402e73f0c075b4eb941fe8b7d358cac4
xlzy <1017866168@qq.com>
2021-04-25 22:41:26 +0800

    d: 删除console

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,13 @@ export default {
       if (uni.canIUse('getUpdateManager')) {
         const updateManager = uni.getUpdateManager()
         updateManager.onCheckForUpdate(function (res) {
-          console.log('onCheckForUpdate====', res) // 请求完新版本信息的回调
-
           if (res.hasUpdate) {
-            console.log('res.hasUpdate====')
             updateManager.onUpdateReady(function () {
               uni.showModal({
                 title: '更新提示',
                 content: '新版本已经准备好，是否重启应用？',
 
                 success(res) {
-                  console.log('success====', res) // res: {errMsg: "showModal: ok", cancel: false, confirm: true}
-
                   if (res.confirm) {
                     // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
                     updateManager.applyUpdate()
