5fd2ad73a901550e81a0aada22ddb9be52ffcaf1
xlzy <1017866168@qq.com>
2021-04-25 22:41:44 +0800

    u: 增加默认图片列表

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,3 +1,51 @@
 export const ActivityStatusEnum = [
   '待审核', '已预约', '已取消', '已参加'
 ]
+
+export const ImgList = [
+  {
+    id: 1,
+    src: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3407686795,968933150&fm=26&gp=0.jpg',
+    sortID: 1,
+  },
+  {
+    id: 2,
+    src: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=137628589,3436980029&fm=26&gp=0.jpg',
+    sortID: 2,
+  },
+  {
+    id: 5,
+    src: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3218418246,4067817241&fm=26&gp=0.jpg',
+    sortID: 5,
+  },
+  {
+    id: 6,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 6,
+  },
+  {
+    id: 3,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 3,
+  },
+  {
+    id: 4,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 4,
+  },
+  {
+    id: 7,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 7,
+  },
+  {
+    id: 8,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 8,
+  },
+  {
+    id: 9,
+    src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2809605157,700908921&fm=26&gp=0.jpg',
+    sortID: 9,
+  }
+]
