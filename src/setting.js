0fcdc739eaedb859feee27060969fabbc131b04f
xlzy <1017866168@qq.com>
2021-05-04 14:31:12 +0800

    u: 增加测试环境

--- src/setting.js
+++ src/setting.js
@@ -1,5 +1,6 @@
 export default {
-  cloudEnv: 'lxx-1gg4sx24e0ca54d5',
+  cloudEnv: 'test-env-7g36xepi5a9ba705', // dev
+  // cloudEnv: 'lxx-1gg4sx24e0ca54d5', // pro
   MAP_KEY: 'TMHBZ-44NKU-RIEVH-26FJG-6XSI6-TYFPG', // 最好自己去申请一个，随时可能会被删除
   baseUrl: '',
 }
