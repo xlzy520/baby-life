969768fb32e65dfac8e4b52deeb1853c29950c54
xlzy <1017866168@qq.com>
2021-05-04 19:18:11 +0800

    u: 完成相册基本样式布局，以及地图跳转

--- src/pages/album/index.vue
+++ src/pages/album/index.vue
@@ -1,28 +1,140 @@
 <template>
   <view class="container">
-    {{title}}
+    <u-search placeholder="时间、地点、天气..." v-model="keyword"></u-search>
+    <view class="">
+      <u-tabs :list="tabList" active-color="#fba414" inactive-color="#606266" :is-scroll="false"
+              height="70" :current="current" @change="change"></u-tabs>
+    </view>
+    <view class="p-2" v-for="(dateItem,index) in dates" :key="dateItem">
+      <view class="pl-2 py-2 layout-slide">
+        <view class="text-black font-medium text-base">{{dateItem}}</view>
+        <view class="text-gray-400 text-xs" @click="chooseLocation(imgMapping[dateItem][0])">
+          {{imgMapping[dateItem][0].location}}
+          <u-icon name="arrow-right" />
+        </view>
+      </view>
+      <view class="grid-imgs">
+        <view class="img-item" v-for="item in imgMapping[dateItem]" :key="item.id">
+          <u-image src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3407686795,968933150&fm=26&gp=0.jpg"
+                   width="165" height="200" :lazy-load="true"></u-image>
+        </view>
+      </view>
+
+    </view>
+    <u-loadmore :status="status" icon-type="flower" :load-text="loadText" />
   </view>
 </template>
 
 <script>
 
+import { dbRequest } from '@/api/common'
+import { getList } from '@/utils/mixins'
+import { fromNowFormat } from '@/utils'
+import setting from '@/setting'
+
 export default {
   components: {
   },
+  mixins: [getList],
+
   data() {
     return {
+      tabList: [
+        {
+          name: '照片',
+        },
+        {
+          name: '视频',
+        }, {
+          name: '图集',
+        }, {
+          name: '回忆',
+        }],
+      current: 0,
       title: 'Hello',
+      imgMapping: {},
+      dates: [],
     }
   },
   onLoad() {
 
   },
-  methods: {
+  onShow() {
 
+  },
+  methods: {
+    chooseLocation(data) {
+      const key = setting.MAP_KEY // 使用在腾讯位置服务申请的key
+      const referer = '萧潇生活' // 调用插件的app的名称
+      const { latitude, longitude } = data.lon_lat
+      const location = JSON.stringify({
+        latitude,
+        longitude,
+      })
+      wx.navigateTo({
+        url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer + '&location' + location,
+      })
+    },
+    change(index) {
+      this.current = index
+    },
+    getList(isRefresh) {
+      uni.showLoading({ title: '获取数据中...' })
+      const skipNum = (this.pageNum - 1) * this.pageSize
+      const actions = [
+        { method: 'orderBy', params: ['createTime', 'desc'] },
+        { method: 'get' }
+      ]
+      if (skipNum) {
+        actions.splice(0, 0, { method: 'skip', params: [skipNum] })
+      }
+      dbRequest('album', actions)
+        .then(res => {
+          console.log(res)
+          this.handleDate(res.data)
+          this.handleResFromMixin(res, v => v, isRefresh)
+        }).catch(() => {
+          this.status = 'nomore'
+          this.noMore = true
+        })
+        .finally(() => {
+          uni.hideLoading()
+          uni.stopPullDownRefresh()
+        })
+    },
+    handleDate(data) {
+      const result = {}
+      const dates = []
+      console.log(data)
+      data.forEach(item => {
+        const date = fromNowFormat(item.createTime)
+        console.log(date)
+        if (!result[date]) {
+          result[date] = []
+          dates.push(date)
+        }
+        result[date].push(item)
+      })
+      this.dates = dates
+      this.imgMapping = result
+      console.log(result)
+    },
+
+  },
+  onReachBottom() {
+
+  },
+  onPullDownRefresh() {
+    this.getList()
   },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.grid-imgs{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 20upx;
+  grid-row-gap: 20upx;
+}
 </style>
