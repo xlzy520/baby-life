cec36b77a086f4eea891cdb968994068aa3cefdb
xlzy <1017866168@qq.com>
2021-05-04 19:16:59 +0800

    u: 调整MAP——KEY

--- src/setting.js
+++ src/setting.js
@@ -1,6 +1,6 @@
 export default {
   cloudEnv: 'test-env-7g36xepi5a9ba705', // dev
   // cloudEnv: 'lxx-1gg4sx24e0ca54d5', // pro
-  MAP_KEY: 'TMHBZ-44NKU-RIEVH-26FJG-6XSI6-TYFPG', // 最好自己去申请一个，随时可能会被删除
+  MAP_KEY: 'OOWBZ-USRCG-ENKQO-IMKFZ-TVFMQ-LVFA2', // 最好自己去申请一个，随时可能会被删除
   baseUrl: '',
 }
